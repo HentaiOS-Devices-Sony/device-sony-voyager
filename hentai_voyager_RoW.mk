f92e77eaedaacca0b275e736022e55fd3207b7ca
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-03-01 19:08:24 -0500

    Create hentai_voyager_RoW.mk

---
+++ hentai_voyager_RoW.mk
@@ -0,0 +1,25 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit device parts
+$(call inherit-product, device/sony/voyager/aosp_h3413.mk)
+
+# Inherit Hentai os vendor 
+$(call inherit-product, vendor/hentai/config/common_telephony.mk)
+
+# Override Product Name
+PRODUCT_NAME := hentai_voyager_RoW
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := voyager
